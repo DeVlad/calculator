867d7dd109f93862bf07c3b2d84cf611069f6e4e
Deyan Vladkov <deian.vladkov@gmail.com>
2018-01-19 19:51:09 +0000

    Added more event listeners and prototypes

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,4 +1,4 @@
-/* Calculator display limit: 15 symbols  */
+/* Calculator without eval() */
 
 var buttons = { // Button : Id
     b0: "btn-0",
@@ -24,10 +24,9 @@ var buttons = { // Button : Id
 
 function Calculator() {
     this.result = 0;
-    this.lastOperation = null;
+    this.lastOperator = null;
     this.lastNum = 0;
-    this.display = '';
-
+    this.osd = '';
     this.initEvents(buttons);
 }
 
@@ -41,31 +40,85 @@ Calculator.prototype.initEvents = function (buttons) {
     document.getElementById(buttons.b6).addEventListener("click", this.getNumber.bind(this, 6));
     document.getElementById(buttons.b7).addEventListener("click", this.getNumber.bind(this, 7));
     document.getElementById(buttons.b8).addEventListener("click", this.getNumber.bind(this, 8));
-    document.getElementById(buttons.b9).addEventListener("click", this.getNumber.bind(this, 9));    
+    document.getElementById(buttons.b9).addEventListener("click", this.getNumber.bind(this, 9));
+
+    document.getElementById(buttons.plus).addEventListener("click", this.add.bind(this));
+    document.getElementById(buttons.minus).addEventListener("click", this.subtract.bind(this));
+    document.getElementById(buttons.multiply).addEventListener("click", this.multiply.bind(this));
+    document.getElementById(buttons.divide).addEventListener("click", this.divide.bind(this));
+
+    document.getElementById(buttons.back).addEventListener("click", this.back.bind(this));
+    document.getElementById(buttons.clear).addEventListener("click", this.clear.bind(this));
+
+    //document.getElementById(buttons.multiply).addEventListener("click", this.getOperator.bind(this, '*'));
 };
 
 Calculator.prototype.getNumber = function (number) {
-    console.log(number);
+    //console.log(number);
+    //this.lastNum = number;    
+    this.osd += number;
+    this.display();
 };
 
-/*
-Calculator.prototype.add = function (input) {    
-    this.result += input;
-    this.lastOperation = '+';
-    return this.result;    
+/*Calculator.prototype.getOperator = function (operator) {
+    if (this.lastOperator === null) {
+        this.lastOperator = operator;
+        this.lastNum = Number(this.osd);        
+    } else {        
+    }    
+   
+    this.display();
 };*/
 
-Calculator.prototype.multiply = function (input) {
+Calculator.prototype.add = function () {
+    console.log('add');
+    this.lastNum = Number(this.osd);
+    this.osd = '';
+    this.display();
 
+    // this.result = this.lastNum + Number(this.ocd);
+    console.log('Result', this.result);
+    this.lastNum = this.osd;
+    console.log('lastNum', this.lastNum);
+    //this.osd = this.result;
+    this.display();
 };
 
-Calculator.prototype.display = function (input) {    
-    if (input == 'C') {
-        document.getElementById('screen').innerHTML = '';
-        this.display = '';
-    }
+Calculator.prototype.subtract = function () {
+    this.lastOperator = '-';
+    console.log('subtract');
+};
 
-    document.getElementById('screen').innerHTML += input;
+Calculator.prototype.multiply = function () {
+    this.lastOperator = '*';
+    console.log('multiply');
+};
+
+Calculator.prototype.divide = function () {
+    this.lastOperator = '/';
+    console.log('divide');
+};
+
+Calculator.prototype.back = function () {
+    this.osd = this.osd.slice(0, -1);
+    this.display();
+};
+
+Calculator.prototype.clear = function () {
+    console.log('clear');
+    this.result = 0;
+    this.lastOperator = null;
+    this.lastNum = 0;
+    this.osd = '';
+    this.display();
+};
+
+Calculator.prototype.display = function () {
+    if (this.osd === '') { // Display zero
+        document.getElementById('screen').innerHTML = this.result;
+    } else {
+        document.getElementById('screen').innerHTML = this.osd;
+    }
 };
 
 var calc = new Calculator();
